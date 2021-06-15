152f0d5702a76a92fbaed57ae9a70afd8bbbc379
almudena-ars <almudena.ars@gmail.com>
2021-06-15 13:51:44 +0200

    Changed hello to goodbye

--- src/com/clase3/main.java
+++ src/com/clase3/main.java
@@ -2,6 +2,6 @@ package com.clase3;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Goodbye world");
     }
 }
