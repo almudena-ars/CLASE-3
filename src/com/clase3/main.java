252dea59264ff652b2fe2624fac614820022a3e8
almudena-ars <almudena.ars@gmail.com>
2021-06-15 14:28:39 +0200

    Create greeter method

--- src/com/clase3/main.java
+++ src/com/clase3/main.java
@@ -3,5 +3,11 @@ package com.clase3;
 public class main {
     public static void main(String[] args) {
         System.out.println("hasta nunca world");
+        System.out.println(greeter("Lucia");
+    }
+
+
+    pulic static String greeter(String name){
+        return "hello" + name;
     }
 }
